6978ff0e5359a0126c189cba2d665341c15fcfa3
felix guglielmi <52172465+felixguglielmiar1@users.noreply.github.com>
2021-06-16 11:52:59 -0300

    Add files via upload

---
+++ meli-sclae - github/milddleware/config.js
@@ -0,0 +1,19 @@
+
+var config = {
+	user: 'sa',
+	password: 'Monorata',
+	server: '191.97.237.83',//'charkomiguel.no-ip.org',
+	database: 'melitest',
+	site_id:'MLA',
+	api_root_url: 'https://api.mercadolibre.com',
+	auth_url: 'http://auth.mercadolibre.com.ar/authorization',
+	oauth_url: 'https://api.mercadolibre.com/oauth/token',
+	user_test: 'https://api.mercadolibre.com/users/test_user',
+	user_me: 'https://api.mercadolibre.com/users/me',
+	datos_faturacion:'https://api.mercadolibre.com/orders/',
+	ordenes_recientes: 'https://api.mercadolibre.com/orders/search?seller=',
+	informacion_cuenta:"https://api.mercadolibre.com/users/me",
+	informacion_envios: 'https://api.mercadolibre.com/shipments/'
+}
+
+module.exports = {config}
---
+++ meli-sclae - github/server.js
@@ -0,0 +1,36 @@
+const express=require('express')
+const app=express()
+var {insertarCodigo}=require('./consultas')
+const port = process.env.PORT || 3000
+
+app.use('/autenticacion',require('./routes/outc'))
+app.use('/generarDatos',require('./routes/datosmeli'))
+ 
+
+app.get('/', async (req, res) => {
+  try{
+    const  code  = req.query.code
+     if (typeof code === "undefined") {}else{await insertarCodigo(code)}
+  
+     res.json({resultado: 'ok'})
+
+    }catch(error){
+      console.log(error)
+      res.json({resultado: "ocurrio un error al optener el codigo"})
+    }
+})
+ 
+app.use(express.static(__dirname + '/assets'))//para enviar cuerpo de formulario
+app.use(express.json)
+/*app.use(function(req, res, next) {
+  res.status(404).redire
+  res.send('404: File Not Found')
+})*/
+
+//app.use(express.urlencoded({extended: true}))
+app.listen(port, () => {
+    console.log('puerto: ' + port)
+})           
+
+
+
